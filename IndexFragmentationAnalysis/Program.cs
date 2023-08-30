b7b16a0a800b0fdd083a88093d8093888654dbc4
Riyadh Hassan <Riyadh.Hassan@frfl.co.uk>
2023-08-30 10:19:47 +0100

    Add project files.

---
+++ IndexFragmentationAnalysis/Program.cs
@@ -0,0 +1,57 @@
+﻿using ConsoleApp2;
+using MassTransit;
+
+var dbContext = new TestDbContext();
+dbContext.Database.EnsureDeleted();
+dbContext.Database.EnsureCreated();
+
+Console.WriteLine("Writing guids... Please wait...");
+for (int i = 0; i < 1000; i++)
+{
+    dbContext.PlainOldGuids.Add(new PlainOldGuid
+    {
+        Id = Guid.NewGuid(),
+        Name = "EfCore"
+    });
+    dbContext.SaveChanges();
+}
+
+for (int i = 0; i < 1000; i++)
+{
+    dbContext.PersonSequentialEfCoreGuidGeneratorMethods.Add(new PersonSequentialEfCoreGuidGeneratorMethod
+    {
+        Name = "EfCore"
+    });
+    dbContext.SaveChanges();
+}
+
+for (int i = 0; i < 1000; i++)
+{
+    dbContext.PersonSequentialSqlServerMethods.Add(new PersonSequentialSqlServerMethod
+    {
+        Name = "Sql"
+    });
+    dbContext.SaveChanges();
+}
+
+for (int i = 0; i < 1000; i++)
+{
+    dbContext.PersonSequentialSqlServerMethodInts.Add(new PersonSequentialSqlServerMethodInt
+    {
+        Name = "Int"
+    });
+    dbContext.SaveChanges();
+}
+
+for (int i = 0; i < 1000; i++)
+{
+    dbContext.PersonSequentialNewIdChrisPattersons.Add(new PersonSequentialNewIdChrisPatterson
+    {
+        Id = NewId.NextGuid(),
+        Name = "ChrisPatterson"
+    });
+    dbContext.SaveChanges();
+}
+
+
+Console.WriteLine("Finished writing guids.");
---
+++ IndexFragmentationAnalysis/TestDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace ConsoleApp2;
+internal class TestDbContext : DbContext
+{
+    public virtual DbSet<PlainOldGuid> PlainOldGuids { get; set; }
+    public virtual DbSet<PersonSequentialEfCoreGuidGeneratorMethod> PersonSequentialEfCoreGuidGeneratorMethods { get; set; }
+    public virtual DbSet<PersonSequentialSqlServerMethod> PersonSequentialSqlServerMethods { get; set; }
+    public virtual DbSet<PersonSequentialSqlServerMethodInt> PersonSequentialSqlServerMethodInts { get; set; }
+    public virtual DbSet<PersonSequentialNewIdChrisPatterson> PersonSequentialNewIdChrisPattersons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=.;database=IndexFragmentationAnalysis;trusted_connection=yes;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /* This is the default value generator for an EFCore guid primary key so it does not need to be explictly set.
+         * Note that it is different to the sql server NEWSEQUENTIALID() value.
+         * Further reading:
+         * https://github.com/dotnet/efcore/blob/main/src/EFCore/ValueGeneration/SequentialGuidValueGenerator.cs
+         * https://github.com/dotnet/efcore/pull/20528
+         * 
+         * modelBuilder.Entity<PersonSequentialEfCoreGuidGeneratorMethod>()
+         *       .Property(x => x.Id)
+         *        .HasValueGenerator<SequentialGuidValueGenerator>();
+         */
+
+        modelBuilder.Entity<PersonSequentialSqlServerMethod>()
+            .Property(x => x.Id)
+            .HasDefaultValueSql("NEWSEQUENTIALID()");
+    }
+}
+
+
+public class PlainOldGuid
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class PersonSequentialEfCoreGuidGeneratorMethod
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class PersonSequentialSqlServerMethod
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class PersonSequentialSqlServerMethodInt
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class PersonSequentialNewIdChrisPatterson
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
